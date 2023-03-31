053ee958a1e5e4d64d15e6636b883d99a8a6a788
dibkb <dibas9110@gmail.com>
2023-03-31 20:14:23 +0530

    get value

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import FullList from "./model/Fulllist";
 import ListItem from "./model/ListItem";
 import ListTemplate from "./templates/ListTemplate";
-function initApp() {}
+function initApp() {
+  const button = document.querySelector("button") as HTMLButtonElement;
+  const form = document.getElementById("form") as HTMLFormElement;
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    const item: string = this.elements[0].value;
+    const quantity: number = Number(this.elements[1]?.value);
+    console.log(item);
+  });
+}
 
 document.addEventListener("DOMContentLoaded", initApp);
