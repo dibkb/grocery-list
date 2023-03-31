b233b660356c7f61b69c63d8915558cf94211a26
dibkb <dibas9110@gmail.com>
2023-03-31 19:08:03 +0530

    Fulllist

--- src/model/Fulllist.ts
+++ src/model/Fulllist.ts
@@ -12,9 +12,38 @@ export default class FulllList implements List {
   constructor(list: ListItem[]) {
     this._list = list;
   }
-  load(): void {}
-  save(): void {}
-  clearList(): void {}
-  addItem(): void {}
-  removeItem(): void {}
+  load(): void {
+    const storedList: string | null = localStorage.getItem("MY_LIST");
+    if (typeof storedList !== "string") return;
+    const parsedList: {
+      _id: string;
+      _item: string;
+      _quantity: number;
+      _checked: boolean;
+    }[] = JSON.parse(storedList);
+    parsedList.forEach((itemObj) => {
+      const newListItem = new ListItem(
+        itemObj._id,
+        itemObj._item,
+        itemObj._quantity,
+        itemObj._checked
+      );
+      this.addItem(newListItem);
+    });
+  }
+  save(): void {
+    localStorage.setItem("MY_LIST", JSON.stringify(this._list));
+  }
+  clearList(): void {
+    this._list = [];
+    this.save();
+  }
+  addItem(item: ListItem): void {
+    this._list.push(item);
+    this.save();
+  }
+  removeItem(id: string): void {
+    this._list.filter((item) => item._id !== id);
+    this.save();
+  }
 }
